a0723850f4b6c7f3a347366d29060c0fc830a0c2
robin <756517553@qq.com>
2022-11-17 09:32:58 +0000

    feat:fix

--- c_algo/hash_impl.c
+++ c_algo/hash_impl.c
@@ -2,25 +2,41 @@
 
 #define CAPACITY 50000 // Size of the Hash Table
 
+unsigned long hash_function(char *str);
+void test_my_hash_idea();
+
+int main()
+{
+  int r = 'a' + 'b';
+  printf("%d \n", r);
+  char *name = "robin_test_abc_deft_robin_asdfasdfasd";
+  unsigned long name_index = hash_function(name);
+  printf("%ld \n", name_index);
+  test_my_hash_idea();
+  return 0;
+}
+
+void test_my_hash_idea()
+{
+  char *arr[1000];
+  printf("out of index:%s \n", arr[999]);
+  char *names[] = {"robin", "advid", "asdf"};
+  for (int i = 0; i < 3; i++)
+  {
+    int sum = 0;
+    for (int j = 0; names[i][j]; j++)
+    {
+      sum += names[i][j];
+    }
+    int index = sum % 1000;
+    arr[index] = names[i];
+  }
+}
+
 unsigned long hash_function(char *str)
 {
   unsigned long i = 0;
   for (int j = 0; str[j]; j++)
     i += str[j];
   return i % CAPACITY;
-}
-
-int main()
-{
-  // int r = 'a' + 'b';
-  // printf("%d", r);
-
-  char *name = "robin_test_abc_deft_robin_asdfasdfasdfkgkasdgkagkgaksgkasgjaskgjkasgjkasgjkasjgkasddgj";
-  unsigned long name_index = hash_function(name);
-  printf("%ld", name_index);
-  return 0;
-}
-
-void test_my_hash_idea(){
-  int char*[];
 }
